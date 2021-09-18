526bcaeb4f1d0146d930ef82cebe80de9f09bd21
a.zhyzhkunets <pochtolyon@gmail.com>
2021-09-18 20:07:13 +0300

    Added folder_manager app

---
+++ apps/folder_manager/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'apps.folder_manager.apps.FolderManagerConfig'
---
+++ apps/folder_manager/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class FolderManagerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.folder_manager'
+    verbose_name = 'Folder Manager'
+
