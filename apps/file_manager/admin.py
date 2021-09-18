e47e64f5ea141dee46f64ca96d8ffa806302ac78
a.zhyzhkunets <pochtolyon@gmail.com>
2021-09-18 20:08:46 +0300

    Added file_manager models, admin

--- apps/file_manager/admin.py
+++ apps/file_manager/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.file_manager.models import File, FilePermission
+
+
+class FilePermissionInline(admin.TabularInline):
+    model = FilePermission
+    extra = 0
+
+
+@admin.register(File)
+class FileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'folder', 'owner', 'created_at', 'id')
+    search_fields = ('name', 'owner__email', 'owner__username')
+    list_filter = ('type', 'created_at', 'modified_at')
+    readonly_fields = ('modified_at', )
+    inlines = [FilePermissionInline]
+
+
+@admin.register(FilePermission)
+class FolderPermissionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'file', 'user', 'permission', 'id')
+    search_fields = ('name', 'user__email', 'user__username')
--- apps/file_manager/models.py
+++ apps/file_manager/models.py
@@ -1,1 +1,56 @@
+from django.contrib.auth.models import Permission
 from django.db import models
+
+from apps.folder_manager.models import Folder
+
+
+class File(models.Model):
+    """
+    User file model
+    """
+    FILE_TYPE_TEXT = 'text'
+    FILE_TYPE_FILE = 'file'
+    FILE_TYPE_LIST = (
+        (FILE_TYPE_TEXT, 'Text'),
+        (FILE_TYPE_FILE, 'File'),
+    )
+
+    name = models.CharField(max_length=255)
+    text = models.TextField(max_length=1000, blank=True)
+
+    type = models.CharField(max_length=15, choices=FILE_TYPE_LIST, default=FILE_TYPE_TEXT)
+    file = models.FileField(max_length=255, upload_to='files', blank=True)
+    folder = models.ForeignKey(Folder, on_delete=models.SET_NULL, related_name='files',
+                               null=True, blank=True)
+    owner = models.ForeignKey('users.User', on_delete=models.SET_NULL, related_name='files',
+                              null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'File'
+        verbose_name_plural = 'Files'
+
+    def __str__(self):
+        return self.name
+
+
+class FilePermission(models.Model):
+    """
+    File permission model
+    """
+
+    name = models.CharField(max_length=255)
+
+    file = models.ForeignKey(File, on_delete=models.CASCADE, related_name='permissions', null=True, blank=True)
+    user = models.ForeignKey('users.User', related_name='file_permissions', on_delete=models.SET_NULL,
+                             blank=True, null=True)
+    permission = models.ForeignKey(Permission, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'File Permission'
+        verbose_name_plural = 'Files Permission'
+
+    def __str__(self):
+        return self.name
