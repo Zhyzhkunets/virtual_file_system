a5f203adcc46b79d7e3275d296cb51a3c558e621
a.zhyzhkunets <pochtolyon@gmail.com>
2021-09-18 20:04:20 +0300

    Added users model, admin

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    Custom user model.
+    """
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    email = models.EmailField(db_index=True, unique=True)
