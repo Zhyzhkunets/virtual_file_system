3274e96e7815691e1da74d4408d48b6fa8b1690d
a.zhyzhkunets <pochtolyon@gmail.com>
2021-09-18 20:07:43 +0300

    Added folder_manager model, admin

---
+++ apps/folder_manager/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+
+from apps.folder_manager.models import Folder, FolderPermission
+
+
+class FolderPermissionInline(admin.TabularInline):
+    model = FolderPermission
+    extra = 0
+
+
+@admin.register(Folder)
+class FolderAdmin(admin.ModelAdmin):
+    list_display = ('name', 'parent', 'owner', 'created_at', 'id')
+    search_fields = ('name', 'owner__email', 'owner__username')
+    list_filter = ('created_at', 'modified_at')
+    readonly_fields = ('modified_at', )
+    inlines = [FolderPermissionInline]
+
+
+@admin.register(FolderPermission)
+class FolderPermissionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'folder', 'user', 'permission', 'id')
+    search_fields = ('name', 'user__email', 'user__username')
---
+++ apps/folder_manager/models.py
@@ -0,0 +1,45 @@
+from django.contrib.auth.models import Permission
+from django.db import models
+
+
+class Folder(models.Model):
+    """
+    User folder model
+    """
+    name = models.CharField(max_length=255)
+
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='children',
+                               null=True, blank=True)
+    owner = models.ForeignKey('users.User', on_delete=models.SET_NULL, related_name='folders',
+                              null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Folder'
+        verbose_name_plural = 'Folders'
+
+    def __str__(self):
+        return self.name
+
+
+class FolderPermission(models.Model):
+    """
+    Folder permission model
+    """
+
+    name = models.CharField(max_length=255)
+
+    folder = models.ForeignKey(Folder, on_delete=models.CASCADE, related_name='permissions',
+                               null=True, blank=True)
+    user = models.ForeignKey('users.User', related_name='folder_permissions', on_delete=models.SET_NULL,
+                             blank=True, null=True)
+    permission = models.ForeignKey(Permission, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Folder Permission'
+        verbose_name_plural = 'Folders Permission'
+
+    def __str__(self):
+        return self.name
