810a5342aaf6476e21769cdec503208c70f19fff
a.zhyzhkunets <pochtolyon@gmail.com>
2021-09-19 17:31:29 +0300

    Added PermissionAdmin to users

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Permission
 
 User = get_user_model()
 
 
 @admin.register(User)
 class AdminUser(UserAdmin):
+    pass
+
+
+@admin.register(Permission)
+class PermissionAdmin(admin.ModelAdmin):
     pass
